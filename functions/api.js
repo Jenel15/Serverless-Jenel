848f9238526c177dc7846b0a15b60d74d1574574
Jenel Ricafrente <125538150+Jenel15@users.noreply.github.com>
2024-05-04 22:18:17 +0800

    Add files via upload

---
+++ functions/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/author');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app= express();
+
+const dbCloudUrl ='mongodb+srv://user1:test123@cluster0.ccomfh0.mongodb.net/user1';
+
+const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+mongoose
+.connect(dbCloudUrl || dbLocalUrl)
+.then(() => console.log('Connecter to MongoDB'))
+.catch((error) => console.error('Failed to connect to mongoDB', error));
+
+app.use('/.netlify/functions/api',router);
+module.exports.handler = serverless(app);
